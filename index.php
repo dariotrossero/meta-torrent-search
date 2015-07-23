434eb30f00550d659a1e614a6e8600a16b088ef7
Dario Trossero <dario.trossero@globant.com>
2015-07-23 12:03:52 -0300

    Check all engines at startup

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 <body>
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="js/materialize.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
 
 <div class="container">
     <?php
